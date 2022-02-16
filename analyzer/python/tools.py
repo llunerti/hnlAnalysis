505f0e27bfe417391dcbf1b3798ba807dfbf0a88
llunerti <leonardo.lunerti@cern.ch>
2022-02-16 11:41:23 +0100

    short dataset handling

--- analyzer/python/tools.py
+++ analyzer/python/tools.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 from datetime import datetime
 
-def update_json_cfg(cat,das_string,output_file_name,input_cfg_json,output_cfg_full_path,max_events,lxplus=True):
+def update_json_cfg(das_string,output_file_name,input_cfg_json,output_cfg_full_path,max_events,lxplus=True):
     output_dir = subprocess.check_output("pwd",shell=True).strip("\n")
     production_time = str(datetime.now())
     
@@ -13,9 +13,12 @@ def update_json_cfg(cat,das_string,output_file_name,input_cfg_json,output_cfg_fu
         out_cfg = json.loads(f.read())
 
     dataset_dic = {}
-    dataset_dic["file_name_list"]    = [str(os.path.join(output_dir,output_file_name))]
-    dataset_dic["production_time"]   = production_time
-    dataset_dic["processed_events"]  = max_events
+    dataset_dic["file_name_list"]   = [str(os.path.join(output_dir,output_file_name))]
+    dataset_dic["production_time"]  = production_time
+    dataset_dic["processed_events"] = max_events
+    cat = str(input_cfg_json[das_string]["dataset_category"])
+    dataset_dic["dataset_category"] = cat
+    dataset_dic["das_string"] = das_string
 
     if cat!="data":
         dataset_dic["cross_section"]     = input_cfg_json[das_string]["cross_section"]
@@ -23,7 +26,8 @@ def update_json_cfg(cat,das_string,output_file_name,input_cfg_json,output_cfg_fu
     else:
         dataset_dic["integrated_lumi"] = -1.0
 
-    out_cfg[cat][das_string] = dataset_dic
+    short_name = str(input_cfg_json[das_string]["short_name"])
+    out_cfg[short_name] = dataset_dic
     
     with open(output_cfg_full_path, "w") as f:
         json.dump(out_cfg,f, indent=4, sort_keys=True)
