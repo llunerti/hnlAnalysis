ce24e83b7f6c354df584bb41ab45888fa00a9f8d
llunerti <leonardo.lunerti@cern.ch>
2022-02-16 16:29:09 +0100

    minor changes

--- analyzer/production/crab18ParkedMC.py
+++ analyzer/production/crab18ParkedMC.py
@@ -21,13 +21,14 @@ with open(in_cfg_full_path,'r') as f:
 #dataset_name = "/QCD_Pt-50to80_MuEnrichedPt5_TuneCP5_13TeV_pythia8/RunIIAutumn18MiniAOD-102X_upgrade2018_realistic_v15-v3/MINIAODSIM"
 dataset_name = '/ParkingBPH1/Run2018D-05May2019promptD-v1/MINIAOD'
 dataset_label = str(dataset_name.split("/")[1])
+ver = 1
 
 config.section_("General")
 config.General.workArea = 'crab_projects'
-config.General.requestName = dataset_label+"_v0"
+config.General.requestName = dataset_label+"_v"+str(ver) 
 if str(input_miniAOD_cfg[dataset_name]["dataset_category"]) == "data":
     dataset_label = str(dataset_name.split("/")[1]+"_"+dataset_name.split("/")[2])
-    config.General.requestName = dataset_label+"_v0"
+    config.General.requestName = dataset_label+"_v"+str(ver)
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
@@ -38,8 +39,10 @@ config.section_("Data")
 config.Data.splitting = 'Automatic'
 if str(input_miniAOD_cfg[dataset_name]["dataset_category"]) == "data":
    config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/13TeV/Legacy_2018/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt" #https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideGoodLumiSectionsJSONFile
-   config.Data.splitting = 'LumiBased'
-   config.Data.unitsPerJob = 15
+   #config.Data.splitting = 'LumiBased'
+   #config.Data.unitsPerJob = 15
+   config.Data.splitting = 'EventAwareLumiBased' #each job will contain a varying number of luminosity sections such that the number of events analyzed by each job is roughly unitsPerJob
+   config.Data.unitsPerJob = 300000 
 config.Data.inputDBS = 'global'
 config.Data.inputDataset = dataset_name
 
